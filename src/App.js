2a59911eb38451af8356b95ed7c41c50574c8db2
无敌暴龙战士 <75242799+lwl-650@users.noreply.github.com>
2022-04-22 10:23:29 +0800

    fix: 国际化完成

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Text from "./page/test.jsx"
+import { React, useState, useEffect } from 'react'
 
 import intl from "react-intl-universal";
-//引入
+require('intl/locale-data/jsonp/en.js');
+require('intl/locale-data/jsonp/zh.js');
+// app locale data
+const locales = {
+  "en_US": require('./page/locales/en_US.json'),
+  "zh_CN": require('./page/locales/zh_CN.json')
+};
 
-//调用
+
+  
+ 
 
 function App() {
+
+  const [initDone, setInitDone] = useState(false)
+  useEffect(() => {
+    loadLocales()
+  }, []);
+
+  const loadLocales = () => {
+    // init method will load CLDR locale data according to currentLocale
+    // react-intl-universal is singleton, so you should init it only once in your app
+    intl.init({
+      // currentLocale: 'en_US', // TODO: determine locale here
+      currentLocale: 'zh_CN',
+      locales,
+    })
+      .then(() => {
+        // After loading CLDR locale data, start to render
+        setInitDone(true)
+      });
+  }
+
   return (
+    initDone &&
     <div className="App">
       <header className="App-header">
-        <div className="mingsheng topItem">
-          {intl.get("influence")}
-          {mingsheng}
-        </div>
+        <h1>这是父组件</h1>
+        {intl.get('title')}
+     <h1><Text></Text></h1>
       </header>
     </div>
   );
