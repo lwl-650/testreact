952a8575c45adb1eaaf5b9cc17554c0864f7bd4a
无敌暴龙战士 <75242799+lwl-650@users.noreply.github.com>
2022-04-22 11:04:46 +0800

    fix：完整版国际化

--- src/App.js
+++ src/App.js
@@ -1,51 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Text from "./page/test.jsx"
-import { React, useState, useEffect } from 'react'
+import Text from "./page/test.jsx";
+import { React, useState, useEffect } from "react";
 
 import intl from "react-intl-universal";
-require('intl/locale-data/jsonp/en.js');
-require('intl/locale-data/jsonp/zh.js');
+require("intl/locale-data/jsonp/en.js");
+require("intl/locale-data/jsonp/zh.js");
 // app locale data
 const locales = {
-  "en_US": require('./page/locales/en_US.json'),
-  "zh_CN": require('./page/locales/zh_CN.json')
+  en_US: require("./page/locales/en_US.json"),
+  zh_CN: require("./page/locales/zh_CN.json"),
 };
 
-
-  
- 
-
 function App() {
-
-  const [initDone, setInitDone] = useState(false)
+  const [initDone, setInitDone] = useState(false);
   useEffect(() => {
-    loadLocales()
+    loadLocales();
   }, []);
-
   const loadLocales = () => {
-    // init method will load CLDR locale data according to currentLocale
-    // react-intl-universal is singleton, so you should init it only once in your app
-    intl.init({
-      // currentLocale: 'en_US', // TODO: determine locale here
-      currentLocale: 'zh_CN',
-      locales,
-    })
+    intl
+      .init({
+        currentLocale: "en_US", // TODO: determine locale here
+        // currentLocale: 'zh_CN',
+        locales,
+      })
       .then(() => {
-        // After loading CLDR locale data, start to render
-        setInitDone(true)
+        setInitDone(true);
       });
-  }
+  };
 
   return (
-    initDone &&
-    <div className="App">
-      <header className="App-header">
-        <h1>这是父组件</h1>
-        {intl.get('title')}
-     <h1><Text></Text></h1>
-      </header>
-    </div>
+    initDone && (
+      <div className="App">
+        <header className="App-header">
+          <h1>这是父组件</h1>
+          {intl.get("title")}
+          <h1>
+            <Text></Text>
+          </h1>
+        </header>
+      </div>
+    )
   );
 }
 
--- src/page/test.jsx
+++ src/page/test.jsx
@@ -10,10 +10,10 @@ export default function Test() {
 
 
   return (
-    // initDone &&
     
     <div>test
        <h1>{intl.get('title')}</h1>
+       <h5>{intl.get('HELLO', { name: 'Tony', where: 'Alibaba' })}</h5>
     </div>
   )
 }
